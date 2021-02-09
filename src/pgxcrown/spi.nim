54cda7bd4954881022b9580bb901606e01456f8b
luisacosta828 <55286192+luisacosta828@users.noreply.github.com>
2021-02-09 12:30:35 -0400

    testing spi_exec

--- src/pgxcrown/spi.nim
+++ src/pgxcrown/spi.nim
@@ -1,7 +1,29 @@
 {. push header: "executor/spi.h".}
 
+type 
+    command* {.importc: "const char*".} = distinct cstring
+
+    OK* {. pure .} = enum
+
+        CONNECT = 1,
+        FINISH, FETCH, UTILITY,
+        SELECT, SELINTO, INSERT,
+        DELETE, UPDATE, CURSOR
+        INSERT_RETURNING, DELETE_RETURNING, UPDATE_RETURNING,
+        REWRITTEN, REL_REGISTER, REL_UNREGISTER, TD_REGISTER
+
+    ERROR* {. pure .} = enum
+        CONNECT = 1,
+        COPY, OPUNKNOWN, UNCONNECTED, 
+        #CURSOR not used anymore
+        ARGUMENT = 6,
+        PARAM, TRANSACTION, NOATTRIBUTE, NOOUTFUNC,
+        TYPEUNKNOWN, REL_DUPLICATE, REL_NOT_FOUND
+
+
 proc connect(): int {. importc: "SPI_connect".}
 proc finish():  int {. importc: "SPI_finish".}
+proc exec*(c: command, count: clong): int {. importc: "SPI_exec".}
 
 template spi_init*(statements: untyped) = 
     var connection_status {.inject.} = connect()
