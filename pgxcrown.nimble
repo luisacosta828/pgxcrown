2ab03387b554416ecd3e62d4ce87af259f9fe110
luisacosta828 <55286192+luisacosta828@users.noreply.github.com>
2021-02-09 11:28:26 -0400

    testing spi_init v0.1.11

--- pgxcrown.nimble
+++ pgxcrown.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.10"
+version       = "0.1.11"
 author        = "luisacosta828"
 description   = "Build Postgres extensions in Nim."
 srcDir        = "src"
--- src/pgxcrown/spi.nim
+++ src/pgxcrown/spi.nim
@@ -4,8 +4,8 @@ proc connect(): int {. importc: "SPI_connect".}
 proc finish():  int {. importc: "SPI_finish".}
 
 template spi_init*(statements: untyped) = 
-    var connection_status = connect()
+    var connection_status {.inject.} = connect()
     statements
-    var finish_status = finish()
+    var finish_status {. inject .} = finish()
 
 {. pop .}
