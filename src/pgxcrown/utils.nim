24b3f5663216632a8e36d7bb1a994a11eea5ddea
luisacosta <luisacosta828@gmail.com>
2021-05-21 18:34:02 -0400

    deleting old plnim dependencies in utils.nim

--- src/pgxcrown/utils.nim
+++ src/pgxcrown/utils.nim
@@ -1,6 +1,6 @@
 include compiler
 include datatypes/basic
 include spi
-include plnim/func_utils
-include plnim/pg_syscache
-include plnim/plnim
+#include plnim/func_utils
+#include plnim/pg_syscache
+#include plnim/plnim
