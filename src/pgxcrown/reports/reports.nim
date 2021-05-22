ac479ac6f1e005c9d9e5bd7f971a8f737de8d02e
luisacosta <luisacosta828@gmail.com>
2021-05-22 00:06:45 -0400

    changing error message ==  to len == 0

--- src/pgxcrown/reports/reports.nim
+++ src/pgxcrown/reports/reports.nim
@@ -2,7 +2,7 @@ import errcodes
 
 template report*(log_strategy, msg: typed, detail_msg: typed = "", hint_msg: typed = "") =
    var strategy_code = log_strategy()
-   if detail_msg != "" and (strategy_code != 19 or strategy_code != 20):
+   if len(detail_msg) == 0 and (strategy_code != 19 or strategy_code != 20):
        echo "strategy: ", strategy_code
        {. emit: [
            """ereport(""",log_strategy(),
