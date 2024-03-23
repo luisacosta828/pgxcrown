b69ee8ee1a99e7c65b7b76c0c176319d04018c40
Luis Acosta <luisacosta828@gmail.com>
2024-03-23 19:29:27 -0400

    removing emit_type_hook declaration

--- src/pgxcrown/hooks/emit_hook.nim
+++ src/pgxcrown/hooks/emit_hook.nim
@@ -1,13 +1,32 @@
-#{.emit: ["""#include "postgres.h""""] .}
+#{.emit: """/*INCLUDESECTION*/
+#include "postgres.h"
+#""".}
+
+{. push header: "utils/elog.h" .}
+
 {.emit: """/*INCLUDESECTION*/
 #include "postgres.h"
 """.}
-{. push header: "utils/elog.h" .}
 
 type
-  ErrorData* {.importc:  "ErrorData" , incompletestruct .} = ref object
-    lineno: int
+  ErrorData* {.importc:  "ErrorData" , incompletestruct .} = object
+    elevel: cint
+    output_to_server: bool
+    output_to_client: bool
+    hide_stmt: bool
+    hide_ctx: bool
+    message: cstring
+    detail: cstring
+    detail_log: cstring
+    hint: cstring
+    context: cstring
+    schema_name: cstring
+    table_name: cstring
+    column_name: cstring
+    datatype_name: cstring
+    constraint_name: cstring
+    lineno: cint
+
+  emit_log_hook_type* {.exportc.} = proc(edata: ptr ErrorData) {. cdecl .}
 
 {.pop.}
-
-
