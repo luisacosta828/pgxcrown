03a3e3e73c9383f32671df8c03071abd4319be6a
Luis Acosta <luisacosta828@gmail.com>
2024-03-26 15:49:33 -0400

    added commandType property

--- src/pgxcrown/hooks/post_parse_hook.nim
+++ src/pgxcrown/hooks/post_parse_hook.nim
@@ -1,8 +1,19 @@
 {.push header: "parser/analyze.h".}
 
 type
+  CmdType* = enum
+    CMD_UNKNOWN
+    CMD_SELECT
+    CMD_UPDATE
+    CMD_INSERT
+    CMD_DELETE
+    CMD_MERGE
+    CMD_UTILITY
+    CMD_NOTHING
+
   ParseState*  {.importc: "ParseState", incompletestruct.}  = object
   Query*       {.importc: "Query", incompletestruct.}       = object
+    commandType*: CmdType
   JumbleState* {.importc: "JumbleState", incompletestruct.} = object
 
 
