313696316b90007f1ae26a4548ae4bad60c24653
Juan Carlos <juancarlospaco@gmail.com>
2024-11-12 12:22:06 -0300

    Nimpretty, style, indent, tiny fixes

--- src/pgxcrown/pgxtool.nim
+++ src/pgxcrown/pgxtool.nim
@@ -1,24 +1,27 @@
 import std/[os, strutils]
 
+
 const available_hooks = ["emit_log", "post_parse_analyze"]
 
+
 proc cli_helper() =
-   echo """
+  echo """
 Usage: pgxtool [command] [options] [target]
 
-Commands: 
-   create-project: initialize a new pgxcrown project
+Commands:
+  create-project: Initialize a new pgxcrown project template to edit
      * pgxtool create-project test
 
-   build-extension: emits a dynamic library that can be loaded into postgres runtime
+  build-extension: Compile a dynamic library that can be loaded into Postgres (.so in Linux, .dll in Windows)
      * pgxtool build-extension test
 
-   create-hook: template for creating postgres hooks
+  create-hook: Initialize a new project template for creating Postgres hooks
      * pgxtool create-hook emit_log
 
-   available-hooks: list hooks supported for pgxcrown
+  available-hooks: List Postgres hooks supported for pgxcrown
      * pgxtool available-hooks
 """
+
 
 template nim_c(module: string): string =
   "nim c -d:entrypoint=" & module & " " & module
@@ -27,20 +30,21 @@ template nim_c(module: string): string =
 template emit_pgx_c_extension(module: string): string =
   "nim c --d:release --app:lib " & module
 
+
 template generate_tmp_file(input_file: string, kind: string = "") =
-  var 
-    pgxcrown_header               = if "hook" in kind: "import pgxcrown/hooks/hook_builder\n\n" else: "import pgxcrown/pgx\n\n"
-    original_content              = readFile(input_file)
-    tmp_content {. inject .}      = pgxcrown_header & '\n' & original_content
-    (dir, file, ext)              = splitFile(input_file)
-    tmp_file {. inject .}         = (dir / ("tmp_" & file & ext))
+  var
+    pgxcrown_header = if "hook" in kind: "import pgxcrown/hooks/hook_builder\n\n" else: "import pgxcrown/pgx\n\n"
+    original_content = readFile(input_file)
+    tmp_content {.inject.} = pgxcrown_header & '\n' & original_content
+    (dir, file, ext) = splitFile(input_file)
+    tmp_file {.inject.} = (dir / ("tmp_" & file & ext))
 
 
 template build_project(req: string, kind: string) =
-  var 
-    source      = req / "src"
+  var
+    source = req / "src"
     entry_point = source / "main.nim"
- 
+
   createDir(req)
   createDir(source)
   writeFile(entry_point, "")
@@ -53,28 +57,30 @@ template build_project(req: string, kind: string) =
 
 
 proc compile2pgx(input_file: string) =
-  generate_tmp_file input_file  
+  generate_tmp_file input_file
   writeFile(tmp_file, tmp_content)
   discard execShellCmd(nim_c(tmp_file))
   discard execShellCmd(emit_pgx_c_extension(tmp_file))
   #removeFile(tmp_file)
 
+
 proc compile2hook(input_file: string) =
   discard execShellCmd(emit_pgx_c_extension(input_file))
 
+
 template build_project_template(req: string, kind: string = "") =
   if dirExists(req):
-    echo "Path in use, choose another name."
+    echo "Path in use, directory already exists, choose another name."
     return
   build_project(req, kind)
 
 
 proc check_command() =
-  var 
+  var
     arg = paramStr(1)
     req = if arg == "available-hooks": "" else: paramStr(2)
 
-  case arg:
+  case arg
   of "create-hook":
     if req in available_hooks:
       build_project_template(req, "hook:" & req)
@@ -90,24 +96,25 @@ proc check_command() =
       else:
         compile2pgx(entry_point)
   of "available-hooks":
-    echo """ 
+    echo """
     * emit_log
     * post_parse_analyze
     """
-    
+  else: cli_helper()
+
 #{.pop.}
 
 
 proc main() =
   let pc = paramCount()
 
-  case pc: 
+  case pc
   of 1, 2: check_command()
   else: cli_helper()
 
   #[
   if pc == 2:
-   var 
+   var
      buildopt = paramStr(1)
      filename = paramStr(2)
 
@@ -116,7 +123,7 @@ proc main() =
        #discard execCmdEx( """ echo " """ & build_pg_function(filename) & """" > """ & nim_target_function & ".sql" )
    else:
        cli_helper()
-  #elif paramCount() == 1: 
+  #elif paramCount() == 1:
   #   var buildopt = paramStr(1)
   #   if buildopt == "--build-plnim-function-handler":
   #      compile_library("~/.nimble/pkgs/pgxcrown-0.4.1/pgxcrown/plnim/plnim")
