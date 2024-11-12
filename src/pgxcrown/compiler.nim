7b251684470d2933e3fcd67b843dc84ab89ca741
Juan Carlos <juancarlospaco@gmail.com>
2024-11-12 20:42:42 -0300

    update

--- src/pgxcrown/compiler.nim
+++ src/pgxcrown/compiler.nim
@@ -1,33 +1,33 @@
 from std/strutils import split
 
 
-
 const
-  root = staticExec("pg_config --includedir").split("\n")[0]
-  pg_config_error = "pg_config is not available"
+  includeDir {.strdefine.} = staticExec("pg_config --includedir").split("\n")[0]
+  libDir {.strdefine.} = staticExec("pg_config --libdir").split("\n")[0]
+  pgVersion {.strdefine.} = staticExec("""psql -V | awk '{ print "/"int($3)""} '""").split("\n")[0]  # pg_config --version ???
+
+
+if includeDir.len == 0:
+  echo "Postgres includedir not found, use -d:includeDir='full/path/to/postgres/include/'"
+if libDir.len == 0:
+  echo "Postgres libDir not found, use -d:libDir='full/path/to/postgres/lib/'"
+if pgVersion.len == 0:
+  echo "Postgres pgVersion not found, use -d:pgVersion='17.0'"
 
 
 when defined(windows):
-    const libdir = staticExec("pg_config --libdir").split("\n")[0]
-    if root == "":
-        echo pg_config_error
-    else:
-        const server = " -I" & root & "/server"
-        const win_32 = server & "/port/win32"
-        const msvc = server & "/port/win32_msvc"
-        const HAVE_LONG_LONG_INT_64 = "HAVE_LONG_LONG_INT_64"
+  const server = " -I" & includeDir & "/server"
+  const win_32 = server & "/port/win32"
+  const msvc = server & "/port/win32_msvc"
+  const HAVE_LONG_LONG_INT_64 = "HAVE_LONG_LONG_INT_64"
 
-        # didn't work on windows (wip)
-        {.passC: "-D" & HAVE_LONG_LONG_INT_64 & win_32 & msvc & server & " -I" & root.}
-        {.passL: "-L" & libdir & " -lpostgres -lpq" .}
+  # didn't work on windows (wip)
+  {.passC: "-D" & HAVE_LONG_LONG_INT_64 & win_32 & msvc & server & " -I" & includeDir.}
+  {.passL: "-L" & libDir & " -lpostgres -lpq" .}
 elif defined(linux):
-    if root == "":
-      echo pg_config_error
-    else:
-      const pg_version: string = staticExec("""psql -V | awk '{ print "/"int($3)""} '""").split("\n")[0]
-      const server = " -I" & root & pg_version & "/server"
-      const internals = " -I" & root & "/internal"
+  const server = " -I" & includeDir & pgVersion & "/server"
+  const internals = " -I" & includeDir & "/internal"
 
-      {.passC: server & internals & " -I" & root.}
+  {.passC: server & internals & " -I" & includeDir.}
 else:
   quit "Unsupported operating system"
