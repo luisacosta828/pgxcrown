1e1f6bff856ddbcb32443824e3cb3c3c8a11c089
Juan Carlos <juancarlospaco@gmail.com>
2024-11-12 13:51:14 -0300

    update

--- src/pgxcrown.nim
+++ src/pgxcrown.nim
@@ -4,27 +4,27 @@ import pgxcrown/reports/reports
 import pgxcrown/hooks/[emit_hook, post_parse_hook]
 
 
-template PG_MODULE_MAGIC* =
-    const DLL* = "PGDLLEXPORT $# $#$#"
-    const V1_DEF* = "PGDLLEXPORT $# $#(PG_FUNCTION_ARGS)"
-    {.pragma: pgdllexport, codegenDecl: DLL, exportc.}
-    {.pragma: pgv1 , codegenDecl: V1_DEF, exportc, dynlib.}
-    {.emit: """PG_MODULE_MAGIC;""" .}
+template PG_MODULE_MAGIC*() =
+  const DLL* = "PGDLLEXPORT $# $#$#"
+  const V1_DEF* = "PGDLLEXPORT $# $#(PG_FUNCTION_ARGS)"
+  {.pragma: pgdllexport, codegenDecl: DLL, exportc.}
+  {.pragma: pgv1 , codegenDecl: V1_DEF, exportc, dynlib.}
+  {.emit: """PG_MODULE_MAGIC;""" .}
+
 
 template PG_FUNCTION_INFO_V1*(funcname: typed) =
-    {.emit: ["""PG_FUNCTION_INFO_V1(""",funcname.astToStr,");"] .}
+  {.emit: ["""PG_FUNCTION_INFO_V1(""", funcname.astToStr, """);"""] .}
 
 
-template ActivateHooks* =
+template ActivateHooks*() =
   {.pragma: user_hook, codegenDecl: "static $1 $2", exportc.}
   {.pragma: original_hook, codegenDecl: "$1 $2", exportc, nodecl.}
   {.pragma: pginitexport, codegenDecl: "PGDLLEXPORT $# _PG_init(void)", exportc.}
 
   {.emit: """/*INCLUDESECTION*/
-#include "postgres.h"
-#include "fmgr.h"
-""".}
-
+    #include "postgres.h"
+    #include "fmgr.h"
+  """.}
 
 
 export reports
