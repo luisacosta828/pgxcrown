0f0e04c27beb87ba707038449604feb1217b779d
Luis Acosta <luisacosta828@gmail.com>
2024-11-28 16:50:30 -0400

    detect result keyword

--- src/pgxcrown/pgxmacros.nim
+++ src/pgxcrown/pgxmacros.nim
@@ -1,7 +1,7 @@
 import std/[macros, strutils]
 
 
-proc NimTypes(dt: string): string =
+template NimTypes(dt: string): string =
   case dt
   of "int", "int32": "cint"
   of "float", "float32": "cfloat"
@@ -10,7 +10,7 @@ proc NimTypes(dt: string): string =
   else: "unknown"
 
 
-proc PgxToNim(dt: string): string =
+template PgxToNim(dt: string): string =
   case dt
   of "int", "int32": "getInt32"
   of "float", "float32": "getFloat4"
@@ -25,28 +25,14 @@ proc ReplyWithPgxTypes(dt: string): string =
   of "float64": "Float8"
   else: "unknown"
 
-
-proc explainWrapper(fn: NimNode): NimNode =
-
-  let pgx_proc = newProc(ident("pgx_" & $fn.name))
-  pgx_proc.params[0] = ident("Datum")
-
-  pgx_proc.pragma = newNimNode(nnkPragma).add(ident("pgv1"))
-
-  let rbody = newTree(nnkStmtList, pgx_proc.body)
-  let fnparams_len = fn.params.len - 1
-  var varSection = newNimNode(nnkVarSection)
-
-  # Declaramos las variables desde las especificaciones de los parametros
+template move_nim_params_as_locals =
   for i in 1..fnparams_len:
     var param = fn.params[i].repr.split(":")
     var pvar = param[0]
     var ptype = param[1].split(" ")[1]
     varSection.add(newIdentDefs(ident(pvar), ident(NimTypes(ptype))))
 
-  varSection.add(newIdentDefs(ident("myres"), ident(NimTypes(fn.params[0].repr))))
-  rbody.add(varSection)
-
+template init_locals_from_datum =
   for i in 1..fnparams_len:
     var param = fn.params[i].repr.split(":")
     var pvar = param[0]
@@ -59,17 +45,51 @@ proc explainWrapper(fn: NimNode): NimNode =
       var getValue = newCall(ident(f), [newIntLitNode(i-1)])
       rbody.add newNimNode(nnkAsgn).add(ident(pvar), getValue)
 
+template copy_fn_body =
   let body_lines = fn.body.len - 2
   for lines in 0..body_lines: rbody.add fn.body[lines]
-  rbody.add newNimNode(nnkAsgn).add(ident("myres"), fn.body[^1])
+
+
+proc explainWrapper(fn: NimNode): NimNode =
+
+  let pgx_proc = newProc(ident("pgx_" & $fn.name))
+  pgx_proc.params[0] = ident("Datum")
+
+  pgx_proc.pragma = newNimNode(nnkPragma).add(ident("pgv1"))
+
+  let rbody = newTree(nnkStmtList, pgx_proc.body)
+  let fnparams_len = fn.params.len - 1
+  var varSection = newNimNode(nnkVarSection)
+
+  move_nim_params_as_locals
+
+  var result_keyword = findChild(fn.body, it.kind == nnkAsgn and it[0].repr == "result")
+
+  if result_keyword == nil:
+    varSection.add(newIdentDefs(ident("myres"), ident(NimTypes(fn.params[0].repr))))
+
+  rbody.add(varSection)
+
+  init_locals_from_datum
+
+  copy_fn_body
+
+  if result_keyword == nil:
+    rbody.add newNimNode(nnkAsgn).add(ident("myres"), fn.body[^1])
 
   var replywith = ident("return" & ReplyWithPgxTypes(fn.params[0].repr))
-  rbody.add newCall(replywith, [ident("myres")])
+  if result_keyword == nil:
+    rbody.add newCall(replywith, [ident("myres")])
+  else:
+    rbody.add newCall(replywith, [fn.body[^1][1]])
+
 
   pgx_proc.body = rbody
 
   when defined(debug): echo pgx_proc.repr
   result = pgx_proc
+
+  echo result.repr
 
 
 macro pgx*(fn: untyped): untyped = explainWrapper(fn)
