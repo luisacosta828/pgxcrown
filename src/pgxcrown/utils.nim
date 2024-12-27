50b4150e529d466875410d181f73332b0cafcd8b
Luis Acosta <luisacosta828@gmail.com>
2024-12-27 15:01:07 -0400

    feat: add utility proc for getting function's oid

--- src/pgxcrown/utils.nim
+++ src/pgxcrown/utils.nim
@@ -1,6 +1,7 @@
 include compiler
 include datatypes/basic
 include spi
-#include plnim/func_utils
-#include plnim/pg_syscache
-#include plnim/plnim
+
+
+proc getFnOid*(fcinfo: FunctionCallInfo): Oid {. inline .} = 
+  fcinfo.flinfo[].fn_oid
