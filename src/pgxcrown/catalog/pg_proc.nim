9a5fdd1ba64a867134d02da11180c2e453875e98
Luis Acosta <luisacosta828@gmail.com>
2025-01-08 13:41:43 -0400

    feat: added utility functions to pg_proc module

--- src/pgxcrown/catalog/pg_proc.nim
+++ src/pgxcrown/catalog/pg_proc.nim
@@ -1,4 +1,4 @@
-from ../datatypes/basic import NameData, Oid, oidvector
+from ../datatypes/basic import NameData, Oid, oidvector, Pointer
 
 {.push header: "catalog/pg_proc_d.h".}
 
@@ -34,4 +34,56 @@ var
   Anum_pg_proc_proargnames* {.importc.}: cuint
 
 {.pop.}
+
+template get_pg_proc_src*(ttuple: typed): cstring =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_prosrc, addr(is_null))
+    content   = TextDatumGetCString(datum)
+  content
+
+template get_pg_proc_name*(ttuple: typed): cstring =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_proname, addr(is_null))
+    fn_name = DatumGetName(datum)
+  NameStr(fn_name[])
+
+template get_pg_proc_kind*(ttuple: typed): char =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_prokind, addr(is_null))
+    fn_kind = DatumGetChar(datum)
+  fn_kind
+
+template get_pg_proc_is_strict*(ttuple: typed): bool =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_proisstrict, addr(is_null))
+    fn_is_strict = DatumGetBool(datum)
+  fn_is_strict
+
+
+template get_pg_proc_nargs*(ttuple: typed): int16 =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_pronargs, addr(is_null))
+    fn_nargs = DatumGetInt16(datum)
+  fn_nargs
+
+template get_pg_proc_default_nargs*(ttuple: typed): int16 =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_pronargdefaults, addr(is_null))
+    fn_default_nargs = DatumGetInt16(datum)
+  fn_default_nargs
+
+template get_pg_proc_argtypes*(ttuple: typed): ptr Oid =
+  var
+    datum = SysCacheGetAttr(PROCOID, ttuple, Anum_pg_proc_proargtypes, addr(is_null))
+    fn_args_types = cast[ptr oidvector](DatumGetPointer(datum))
+  fn_args_types.values
+
+template asIntptr(vector: ptr Oid): int = cast[int](vector)
+
+template asOid*(address: int): Oid =
+  (cast[ptr Oid](address))[]
+
+proc `+`*(vector: ptr Oid, offset: int): int {.inline.} = 
+  vector.asIntptr + (sizeof(Oid) * offset)
+
 
