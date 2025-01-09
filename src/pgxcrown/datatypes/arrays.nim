f091026a290c0de8635e230785ff70aacb72e33e
Luis Acosta <luisacosta828@gmail.com>
2025-01-08 23:07:20 -0400

    fix: change header name

--- src/pgxcrown/datatypes/arrays.nim
+++ src/pgxcrown/datatypes/arrays.nim
@@ -1,9 +1,12 @@
-from basic import Oid
-{.push header: "utils/arrays.h".}
+from basic import Oid, Datum
+{.push header: "utils/array.h".}
 type
   ArrayType*  {.importc.} = object
     vl_len {.importc: "vl_len_".}: int32
     ndim: int
     dataoffset: int32
     elemtype: Oid
+
+proc DatumGetArrayTypeP*(x: Datum): ptr ArrayType {.importc.}
+proc deconstruct_array_builtin*(arr: ptr ArrayType, elemtype: Oid, elemsp: ptr ptr Datum, nullsp: ptr ptr bool, nelemsp: var int) {.importc.}
 {.pop.}
