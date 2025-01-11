8d70872751009c6aa6c1440c343d39bd67c8f699
Luis Acosta <luisacosta828@gmail.com>
2025-01-11 19:12:27 -0400

    feat: initial array support

--- src/pgxcrown/datatypes/arrays.nim
+++ src/pgxcrown/datatypes/arrays.nim
@@ -1,12 +1,12 @@
-from basic import Oid, Datum
 {.push header: "utils/array.h".}
 type
   ArrayType*  {.importc.} = object
-    vl_len {.importc: "vl_len_".}: int32
-    ndim: int
-    dataoffset: int32
-    elemtype: Oid
+    vl_len* {.importc: "vl_len_".}: int32
+    ndim*: int
+    dataoffset*: int32
+    elemtype*: cuint
 
-proc DatumGetArrayTypeP*(x: Datum): ptr ArrayType {.importc.}
-proc deconstruct_array_builtin*(arr: ptr ArrayType, elemtype: Oid, elemsp: ptr ptr Datum, nullsp: ptr ptr bool, nelemsp: var int) {.importc.}
+proc DatumGetArrayTypeP*(x: cuint): ptr ArrayType {.importc.}
+proc deconstruct_array_builtin*(arr: ptr ArrayType, elemtype: cuint, elemsp: ptr ptr cuint, nullsp: ptr ptr bool, nelemsp: var int) {.importc.}
+proc deconstruct_array*(arr: ptr ArrayType, elemtype: cuint, elmlen: cint, elmbyval: bool, elmalign: char, elemsp: ptr ptr cuint, nullsp: ptr ptr bool, nelemsp: var int) {.importc.}
 {.pop.}
