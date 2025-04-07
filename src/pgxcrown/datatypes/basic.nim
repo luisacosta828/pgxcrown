ae7cba21bb671944e26f7b0538a6e00c89d60af6
Luis Acosta <luisacosta828@gmail.com>
2025-04-07 16:23:32 -0400

    feat: adding more converters

--- src/pgxcrown/datatypes/basic.nim
+++ src/pgxcrown/datatypes/basic.nim
@@ -2,7 +2,7 @@
 {.push header: "postgres.h".}
 
 type
-  Datum* {.importc: "Datum".} = cuint
+  Datum* {.importc: "Datum".} = uint64
   
   Text* {.importc: "text".} = object
     vl_len {.importc: "vl_len_".}:   array[4, char]
@@ -36,9 +36,11 @@ type
 proc cstring_to_text*(s: const_char): Text {.importc.}
 proc NameStr*(name: NameData): cstring {.importc.}
 proc DatumGetObjectId*(datum_id: Datum): Oid {.importc.}
+proc DatumGetInt64*(x: Datum): int64 {.importc.}
 proc DatumGetInt32*(x: Datum): int32 {.importc.}
 proc DatumGetInt16*(x: Datum): int16 {.importc.}
 #proc DatumGetInt8*(x: Datum): int8 {.importc.}
+proc DatumGetUInt64*(x: Datum): uint64 {.importc.}
 proc DatumGetUInt32*(x: Datum): uint32 {.importc.}
 proc DatumGetUInt16*(x: Datum): uint16 {.importc.}
 proc DatumGetUInt8*(x: Datum): uint8 {.importc.}
@@ -50,9 +52,11 @@ proc DatumGetCString*(x: Datum): cstring {.importc.}
 proc DatumGetName*(x: Datum): Name {.importc.}
 
 proc ObjectIdGetDatum*(id: Oid): Datum {.importc.}
+proc Int64GetDatum*(x: int64): Datum {.importc.}
 proc Int32GetDatum*(x: int32 | int ): Datum {.importc.}
 proc Int16GetDatum*(x: int16 ): Datum {.importc.}
 proc Int8GetDatum*(x: int8 ): Datum {.importc.}
+proc UInt64GetDatum*(x: uint64): Datum {.importc.}
 proc UInt32GetDatum*(x: uint32 | uint): Datum {.importc.}
 proc UInt16GetDatum*(x: uint16): Datum {.importc.}
 proc UInt8GetDatum*(x: uint8): Datum {.importc.}
@@ -101,6 +105,8 @@ template getFnOid*(fcinfo: FunctionCallInfo): Oid =
 
 # Get argument type value declaration
 
+proc getInt64*(value: cuint): clonglong {.importc: "PG_GETARG_INT64".}
+
 proc getInt32*(value: cuint): cint {.importc: "PG_GETARG_INT32".}
 
 proc getUInt32*(value: cuint): cuint {.importc: "PG_GETARG_UINT32".}
@@ -120,6 +126,8 @@ proc getFloat8*(value: cuint): cdouble {.importc: "PG_GETARG_FLOAT8".}
 proc DatumGetTextPP*(value: Datum): ptr Text {.importc.}
 
 proc getOid*(value: cuint): Oid {.importc: "PG_GETARG_OID".}
+
+proc getChar*(value: cuint): cchar {.importc:  "PG_GETARG_CHAR".}
 
 # Return types declaration
 
@@ -157,9 +165,11 @@ proc DirectFunctionCall9*(fn: PGType, arg1: Datum, arg2: Datum, arg3: Datum, arg
 {.pop.}
 
 converter DatumToObjectId*(x: Datum): Oid = DatumGetObjectId(x) 
+converter DatumToInt64*(x: Datum): int64 =  DatumGetInt64(x)
 converter DatumToInt32*(x: Datum): int =  DatumGetInt32(x)
 converter DatumToInt16*(x: Datum): int16 = DatumGetInt16(x)
-converter DatumToUInt32*(x: Datum): uint = DatumGetUInt32(x)
+converter DatumToUInt64*(x: Datum): uint64 =  DatumGetUInt64(x)
+converter DatumToUInt32*(x: Datum): uint32 | uint = DatumGetUInt32(x)
 converter DatumToUInt16*(x: Datum): uint16 = DatumGetUInt16(x)
 converter DatumToUInt8*(x: Datum): uint8 = DatumGetUInt8(x)
 converter DatumToChar*(x: Datum): cchar = DatumGetChar(x)
@@ -170,9 +180,11 @@ converter DatumToCString*(x: Datum): cstring = DatumGetCString(x)
 converter DatumToName*(x: Datum): Name = DatumGetName(x)
 
 converter ObjectIdToDatum*(value: Oid): Datum = ObjectIdGetDatum(value)
-converter Int32ToDatum*(value: int32 | int): Datum = Int32GetDatum(value)
+converter Int64ToDatum*(value: int64): Datum = Int64GetDatum(value)
+converter Int32ToDatum*(value: int | int32): Datum = Int32GetDatum(value)
 converter Int16ToDatum*(value: int16): Datum = Int16GetDatum(value)
-converter UInt32ToDatum*(value: uint): Datum = UInt32GetDatum(value)
+converter UInt64ToDatum*(value: uint64): Datum = UInt64GetDatum(value)
+converter UInt32ToDatum*(value: uint32 | uint): Datum = UInt32GetDatum(value)
 converter UInt16ToDatum*(value: uint16): Datum = UInt16GetDatum(value)
 converter UInt8ToDatum*(value: uint8): Datum = UInt8GetDatum(value)
 converter CharToDatum*(value: cchar): Datum =  CharGetDatum(value) 
@@ -181,3 +193,5 @@ converter Float4GetDatum*(value: cfloat | float32 | float): Datum = Float4GetDat
 converter PointerToDatum*(value: Pointer): Datum = PointerGetDatum(value)
 converter CStringToDatum*(value: cstring): Datum = CStringGetDatum(value)
 converter NameToDatum*(value: Name): Datum = NameGetDatum(value)
+converter CstringToNimString*(value: cstring): string = $value
+converter StringToDatum*(value: string):Datum = cstring(value)
