967e0065a39a0973c51fc3f694afddfbb753bc1c
Luis Acosta <luisacosta828@gmail.com>
2025-06-05 21:41:29 -0400

    wip: select statement

--- src/pgxcrown/query_builder.nim
+++ src/pgxcrown/query_builder.nim
@@ -1,25 +1,145 @@
+import std/strutils
+import std/tables
+import std/sequtils
+
 type
-  command {.importc: "const char*", used.} = distinct cstring
+  ColumnKind = enum
+    single
+    custom
+    subquery
 
-  QueryBuilder = object
-    columns: seq[string]
-    table: seq[string]
-    where: seq[string]
+  JoinKind = enum
+    LeftJoin
+    RightJoin
+    InnerJoin
+
+  SelectBuilder = object
+    columns: seq[Column]
+
+  FromBuilder = object
+    stmt: SelectBuilder
+    table: string
+    alias: string
+    join_cols: TableRef[string, bool]
+
+  JoinBuilder = object
+    table: string
+    alias: string
+    join_cols: TableRef[string, bool]
+     
+  SingleColumn = object
+    prop: string
+
+  CustomColumn = object
+    prop: (string, string)
+
+  Column = object
+    case kind: ColumnKind
+    of single: single: SingleColumn
+    of custom: custom: CustomColumn
+    of subquery: 
+      subquery: SelectBuilder
+      name: string
+
+  Builders = SelectBuilder | FromBuilder | JoinBuilder
+  
+template single(name: string): Column = 
+  Column(kind: single, single: SingleColumn(prop: name)) 
+
+template custom(name: (string, string)): Column = 
+  Column(kind: custom, custom: CustomColumn(prop: name)) 
+
+proc subquery*(query: SelectBuilder, name: string = ""): Column =
+  assert query.columns.len == 1, "Syntax Error: Subquery cannot return more than one column."
+  Column(kind: subquery, subquery: query, name: name) 
+
+proc `$`*(s: SelectBuilder): string =
+  result = "SELECT "
+  var col_seq:seq[string]
+  for col in s.columns:
+    col_seq.add case col.kind:
+    of single: col.single.prop
+    of custom: col.custom.prop[0] & " " & col.custom.prop[1]
+    of subquery: 
+      "( " & $col.subquery & " ) " & col.name
+
+  result.add col_seq.join(", ")
+
+proc `$`*(f: FromBuilder): string =
+  result = $f.stmt & "\nFROM " & f.table & " " & f.alias
 
 
-proc From(query: var QueryBuilder, table: seq[string]): var QueryBuilder {.inline.} =
-  query.table = table
-  result = query
+converter stringToColumn(col:string):Column =
+  if " as " in col:
+    var tup = col.split(" as ")
+    custom((tup[0].strip, tup[1].strip))
+  else:
+    single(col.strip)
 
+converter SelectBuilderToColumn(col: SelectBuilder): Column =
+  subquery(col)
 
-proc Select(query: var QueryBuilder, columns: seq[string]): QueryBuilder {.inline.} =
-  query.columns = columns
-  result = query
+proc `as`*(col: SelectBuilder, subquery_name: string): Column =
+  subquery(col, subquery_name)
 
+proc Select*(cols: varargs[Column]): SelectBuilder = 
+  result = SelectBuilder(columns: cols.toSeq)
 
-var query = QueryBuilder()
-var column: seq[string] = @["one"]
+proc From*(stmt: SelectBuilder, table: string, alias: string = ""): FromBuilder =
+  var col_name: string
 
+  result = FromBuilder(stmt: stmt, table: table, alias: alias, join_cols: newTable[string, bool]())
 
-echo query.From(@["pg_constraint"]).Select(column)
-#proc build(query: QueryBuilder): const_char {. inline .} = const_char(query.table)
+  if alias.strip.len != 0:
+    for c in stmt.columns:
+      col_name = case c.kind:
+      of single: c.single.prop
+      of custom: c.custom.prop[0]
+      else: ""
+
+      if "." in col_name:
+        # it must be a column defined from a join clause
+        if (alias & ".") notin col_name:
+          result.join_cols[col_name] = false
+        
+proc Join*(stmt: FromBuilder | JoinBuilder, kind: JoinKind, table: string, alias: string): JoinBuilder =
+  for k,v in stmt.join_cols.mpairs:
+    if (alias & ".") in k:
+      stmt.join_cols[k] = true
+    else:
+      stmt.join_cols[k] = false
+
+  echo $stmt.join_cols      
+  result = JoinBuilder()
+
+proc on*(stmt: JoinBuilder, predicate: string):string = discard
+
+proc Where*(stmt: FromBuilder | JoinBuilder): string = discard
+
+proc execute*(stmt: Builders) =
+  discard
+
+proc myquery:auto =
+    Select("1 as r2") as "custom_name" # subquery with custom name 
+
+proc my_proc() =
+  var 
+    extracted_subquery = Select("1 as r2") as "custom_name" # subquery with custom name
+    result = Select(
+      "j", # single column name
+      "a.b",# table alias  
+      "a    as    m", # custom name
+      Select("1 as r"), # subquery
+      Select("1 as r2") as "custom_name", # subquery with custom name
+      extracted_subquery,
+      myquery
+    )
+    
+ #   Select(single("j.r"), single("m.m"), single("jara"))
+ #   .From("results", "r")
+ #   .Join(InnerJoin, "results", "j")
+
+ # result.execute()
+  echo result
+
+my_proc()
