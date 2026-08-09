caf3ca78f0f108207bd74f198361f0b301ebc6b4
Luis Acosta <luisacosta828@gmail.com>
2026-08-08 22:35:12 -0400

    feat: add reportError template for controlled ereport(ERROR) transaction aborts

--- src/pgxcrown/reports/reports.nim
+++ src/pgxcrown/reports/reports.nim
@@ -1,12 +1,18 @@
 import errcodes
 
 template report*(log_strategy, msg: typed) =
-
   {. emit: [
       """ereport(""",
       log_strategy(),
       """, (errmsg("%s", """,
       msg.astToStr,
+      """)));"""
+    ].}
+
+template reportError*(msg: string) =
+  {. emit: [
+      """ereport(ERROR, (errmsg("%s", """,
+      msg,
       """)));"""
     ].}
 
